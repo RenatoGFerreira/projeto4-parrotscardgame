87f7d3381075501b6deb3f69dc50c2e332b8e34b
Renato Ferreira <renatoyferreira@gmail.com>
2022-08-19 21:57:50 -0300

    finalizado projeto

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let resposta;
+let simNao;
 let totJogadas = 0;
 let fimGame = 0;
 let stageGame = document.querySelector('.stage-game')
 let cardOne;
 let cardTwo;
+const timer = document.querySelector('.timer')
 const parrots = [
     'bobrossparrot',
     'explodyparrot',
@@ -16,58 +18,24 @@ const parrots = [
 playGame()
 createCard()
 
-function revelaCard(objeto){
+function startTimer(){
+    this.item = setInterval(() =>{
+        const tempo = Number(timer.innerHTML)
 
-    totJogadas ++
-    console.log(`clique: ${totJogadas}`)
-    
-    if(cardOne === undefined){
-        objeto.target.parentNode.classList.add('mostra-carta')
-        cardOne = objeto.target.parentNode
+        timer.innerHTML = tempo + 1
 
-    } else if(cardTwo === undefined){
-        objeto.target.parentNode.classList.add('mostra-carta')
-        cardTwo = objeto.target.parentNode
-        
-        compareCard(cardOne, cardTwo)
-    }
+
+    }, 1000);
+
 
 }
-
-function compareCard(a, b){
-    const firstCard = a.getAttribute('data-parrot')
-    const secondCard = b.getAttribute('data-parrot')
-
-    if( firstCard === secondCard){
-
-            cardOne = undefined
-            cardTwo = undefined
-
-            fimGame++
-            if(fimGame == resposta){
-                setTimeout(()=>{
-                    window.alert(`Você ganhou em ${totJogadas} jogadas com tempo de Tempo.`)
-                },1200)
-            }
-            
-
-    }else{
-
-        setTimeout(()=>{
-            cardOne.classList.remove('mostra-carta')
-            cardTwo.classList.remove('mostra-carta')  
-            
-            cardOne = undefined
-            cardTwo = undefined
-        }, 1000)
-    }
-}
-
 
 function playGame(){   
     while((resposta % 2 !== 0 || resposta > 14) || (resposta % 2 !== 0 || resposta < 4) || resposta === undefined){
         resposta = window.prompt("Por favor, digite a quantidade de cartas \n(Valores pares de 4 a 14):")    
     }  
+
+    startTimer()
 
     resposta = resposta/2
     const parrotsGame = []
@@ -110,6 +78,58 @@ function createCard(parrot){
 }
 
 
+function revelaCard(objeto){
 
+    totJogadas ++
+    console.log(`clique: ${totJogadas}`)
+    
+    if(cardOne === undefined){
+        objeto.target.parentNode.classList.add('mostra-carta')
+        cardOne = objeto.target.parentNode
+
+    } else if(cardTwo === undefined){
+        objeto.target.parentNode.classList.add('mostra-carta')
+        cardTwo = objeto.target.parentNode
+        
+        compareCard(cardOne, cardTwo)
+    }
+
+}
+
+function compareCard(a, b){
+    const firstCard = a.getAttribute('data-parrot')
+    const secondCard = b.getAttribute('data-parrot')
+
+    if( firstCard === secondCard){
+
+            cardOne = undefined
+            cardTwo = undefined
+
+            fimGame++
+            if(fimGame == resposta){
+                setTimeout(()=>{
+                    clearInterval(this.item)
+
+                    simNao = window.confirm(`Você ganhou em ${totJogadas} jogadas e o seu tempo foi ${timer.innerHTML}s.\nDeseja jogar novamente? [Ok/Cancelar]`)
+
+                    if(simNao === true){
+                        location.reload()
+                    }
+                    
+                },1200)
+            }
+            
+
+    }else{
+
+        setTimeout(()=>{
+            cardOne.classList.remove('mostra-carta')
+            cardTwo.classList.remove('mostra-carta')  
+            
+            cardOne = undefined
+            cardTwo = undefined
+        }, 1000)
+    }
+}
 
 
